bc092af0cd9a9b92b362ddff0f05c4aa05e9cffb
YaelAguilar <213452@ids.upchiapas.edu.mx>
2024-04-15 05:15:44 -0600

    [UPDATE] folders has ben renamed

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app.use(
 );
 
 // use express router
-app.use("/", require("./routes"));
+app.use("/", require("./src/infrastructure/routes"));
 
 app.listen(process.env.PORT || 8000, (err) => {
   if (err) {
